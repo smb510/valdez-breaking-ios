e13b74938ede5bb616362d5e485896419964aaea
Scottie Biddle <smb510@gmail.com>
2015-04-13 22:30:12 -0700

    Initial Commit

---
+++ BreakingNewsValdez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BreakingNewsValdez
+//
+//  Created by Scott Biddle on 4/13/15.
+//  Copyright (c) 2015 Sbunticot Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
